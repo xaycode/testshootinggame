ac4fbe23c75ede4eb578506a26eee5f579cac341
xaycode <43464027+xaycode@users.noreply.github.com>
2019-03-11 14:55:51 +0700

    Add files via upload

---
+++ material/tembakan.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tembakan : MonoBehaviour 
+{
+
+	public Rigidbody pelurunya;
+	public Transform posisiTembak;
+	public float powernya = 1000f;
+	public float kecepatanpeluru = 10f;
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal") * Time.deltaTime * kecepatanpeluru;
+		float v = Input.GetAxis ("Vertical") * Time.deltaTime * kecepatanpeluru;
+		transform.Translate (new Vector3 (h, v, 0));
+		if (Input.GetButtonUp ("Fire1")) {
+			Rigidbody peluru = Instantiate (pelurunya, posisiTembak.position, posisiTembak.rotation) as Rigidbody;
+			peluru.AddForce (posisiTembak.forward * powernya);
+		}
+	}
+}
